7fc2d5d7c41fac64f018d2ebb5c0110fc0b04218
sujit1998 <sujit143chaudhary@gmail.com>
2017-02-25 16:38:33 +0530

    truncate

---
+++ D-mine-4/queryExecutor.php
@@ -0,0 +1,106 @@
+<?php
+//server connection script
+include 'dbconnect.php';
+
+//tag which is fetch from sender
+$tags=$_POST['tag'];
+
+//main logic
+switch ($tags)
+{
+case "createDatabase":
+
+    $DBName=$_POST['dbName'];
+    $sql = "CREATE DATABASE $DBName";
+    if ($conn->query($sql) === TRUE) {
+      echo "Database created successfully";
+    } else {
+      echo "Error creating database: " . $conn->error;
+    }
+    $conn->close();
+    break;
+
+case "selectDB":
+    $DBName=$_POST['dbName'];
+    if($conn->select_db($DBName))
+    {
+      echo "Database selected successfully..!";
+    }
+    else {
+      echo "Database is not selected..!";
+    }
+    break;
+
+case "createTable":
+    $DB_Name=$_POST['db_Name'];
+    $query_Stat=$_POST['Query'];
+    if($conn->select_db($DB_Name))
+    {
+      $sql = $query_Stat;
+      if ($conn->query($sql) === TRUE) {
+          echo "Table created successfully";
+      } else {
+          echo "Error creating table: " . $conn->error;
+      }
+    }
+    else {
+      echo "Database is not selected..!";
+    }
+    break;
+
+case "DropTable":
+	$DB_Name=$_POST['dbName'];
+	$query_Stat=$_POST['Query'];
+	if($conn->select_db($DB_Name))
+    {
+      $sql = $query_Stat;
+      if ($conn->query($sql) === TRUE) {
+          echo "Table dropped successfully";
+      } else {
+          echo "Error dropping table: " . $conn->error;
+      }
+    }
+    else {
+      echo "Database is not selected..!";
+    }
+    break;
+	
+	
+case "DropDatabase":
+	$DB_Name=$_POST['dbName'];
+	$query_Stat=$_POST['Query'];
+	if($conn->select_db($DB_Name))
+    {
+      $sql = $query_Stat;
+      if ($conn->query($sql) === TRUE) {
+          echo "Database dropped successfully";
+      } else {
+          echo "Error dropping Database: " . $conn->error;
+      }
+    }
+    else {
+      echo "Database does not exist..!";
+    }
+    break;
+    case "truncateTable":
+    $db_name=$_POST['dbName'];
+    $tb_name=$_POST['tbName'];
+    $query=$_POST['Query'];
+    if($conn->select_db($db_name))
+    {
+      $sql=$query;
+      if($conn->query($sql)===true)
+      {
+        echo "Truncate Table successfully";
+      }
+    }
+    else
+    {
+      echo "Table not Truncated";
+    }
+    break;
+default:
+    echo "Nothing to show";
+}
+
+?>
